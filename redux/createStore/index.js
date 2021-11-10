b9b6a96c71e71b1cecea61f83ddbe3a14c0e09d1
Petro Babiuk <petro.babiuk2506@gmail.com>
2021-11-11 00:18:34 +0200

    add/ add createStore

---
+++ redux/createStore/index.js
@@ -0,0 +1,21 @@
+const createStore = (reducer, initialState) => {
+    let currentReducer = reducer;
+    let currentState = initialState;
+    let listener = () => { };
+
+    return {
+        dispatch(action) {
+            currentState = currentReducer(currentState, action);
+            listener()
+            return action;
+        },
+        subscribe(newListener) {
+            listener = newListener;
+        },
+        getState() {
+            return currentState;
+        }
+    };
+}
+
+module.exports = createStore;
